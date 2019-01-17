b5bd7e7d541ada28796ebb15b113c1fa66fab052
vojtek <vojtekkol@gmail.com>
2019-01-17 18:33:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup
 setup(name='queuing',
       version='0.2.1',
       description='Multithreating producent-consumer solution',
-      url='http://github.com/',
+      url='https://github.com/vojtek/queuing.git',
       author='Wojciech Kolodziej',
       author_email='vojtekkol@o2.pl',
       license='MIT',
-      packages=['queuing'],
-      zip_safe=False)
+      packages=setuptools.find_packages(),
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ])
